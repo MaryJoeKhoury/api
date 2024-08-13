edf24db0f7f4c44e1b733d0a13eb382188dcb7c6
Mary Joe <maryjoekhoury@outlook.com>
2024-08-13 12:32:48 +0300

    changed style inside id's page

--- pages/about/[id].js
+++ pages/about/[id].js
@@ -32,30 +32,24 @@ export default function Page({ data }) {
       <NavBar />
 
       <AlignContainer>
-        <div className="text-white flex flex-col items-center justify-center gap-10 flex-wrap">
-          {/* <div>{post.vimeo_id}</div> */}
-          <div className="flex flex-row justify-between flex-wrap">
+        <div className="text-white flex flex-col items-center justify-center gap-10 flex-wrap max-w-4xl m-auto">
+          <img src={post.image} className="rounded-3xl" />
+          <h1 className="text-xl">{post.title}</h1>
+          <div className="flex flex-row justify-between flex-wrap font-semibold px-8">
             <div className="text-2xl w-1/2">{post.text}</div>
-            <div className="flex flex-col ">
-              <div className="text-2xl">
-                <span className="text-[#00a3bb]">Client</span>:{post.client}
-              </div>
-              <div className="text-2xl">
-                <span className="text-[#00a3bb]">Directors</span>:
-                {post.directors}
-              </div>
-              <div className="text-2xl">
-                <span className="text-[#00a3bb]">Agency</span>:{post.agency}
-              </div>
+            <div className="flex flex-col gap-2 ">
+              <div className="text-2xl">CLIENT: {post.client}</div>
+              <div className="text-2xl">DIRECTOR(S): {post.directors}</div>
+              <div className="text-2xl">DP: {post.cinematographer}</div>
+              <div className="text-2xl">AGENCY: {post.agency}</div>
             </div>
           </div>
-          <div className="text-[#fe424c] text-4xl">Extra data</div>
-          <div>
-            <div>{post.cinematographer}</div>
+          {/* <div className="text-[#fe424c] text-4xl">Extra data</div> */}
+          {/* <div>
             <div>{post.colorist}</div>
             <div>{post.seo_title}</div>
             <div>{post.seo_description}</div>
-          </div>
+          </div> */}
         </div>
       </AlignContainer>
 
