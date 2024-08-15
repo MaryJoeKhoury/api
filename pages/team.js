95b455f98483084fa438b432baceb1f4b291264a
Mary Joe <maryjoekhoury@outlook.com>
2024-08-15 11:40:00 +0300

    add view more button

--- pages/team.js
+++ pages/team.js
@@ -35,12 +35,29 @@ const cardInfo = [
   { id: "3", image: "/images/coding.webp", title: "music", type: "music" },
   { id: "4", image: "/images/coding.webp", title: "art", type: "art" },
   { id: "5", image: "/images/coding.webp", title: "sport", type: "sport" },
+  { id: "6", image: "/images/coding.webp", title: "sport", type: "sport" },
+  { id: "7", image: "/images/coding.webp", title: "coding", type: "tech" },
+  { id: "8", image: "/images/coding.webp", title: "coding", type: "tech" },
+  { id: "9", image: "/images/coding.webp", title: "music", type: "music" },
+  { id: "10", image: "/images/coding.webp", title: "art", type: "art" },
+  { id: "11", image: "/images/coding.webp", title: "sport", type: "sport" },
+  { id: "12", image: "/images/coding.webp", title: "sport", type: "sport" },
+  { id: "13", image: "/images/coding.webp", title: "sport", type: "sport" },
+  { id: "14", image: "/images/coding.webp", title: "coding", type: "tech" },
+  { id: "15", image: "/images/coding.webp", title: "coding", type: "tech" },
+  { id: "16", image: "/images/coding.webp", title: "music", type: "music" },
+  { id: "17", image: "/images/coding.webp", title: "art", type: "art" },
+  { id: "18", image: "/images/coding.webp", title: "sport", type: "sport" },
+  { id: "19", image: "/images/coding.webp", title: "sport", type: "sport" },
 ];
 
 export default function Team() {
   const [activeIndex, setActiveIndex] = useState(null);
   const [filteredType, setFilteredType] = useState(cardInfo);
   const [selectedType, setSelectedType] = useState("all");
+  // const [displayedData, setDisplayedData] = useState(filteredType);
+  const [visibleItemCount, setVisibleItemCount] = useState(6);
+
   const handleItemClick = (index) => {
     setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
   };
@@ -52,6 +69,7 @@ export default function Team() {
   const handleType = (e) => {
     let type = e.target.value;
     setSelectedType(type);
+    setVisibleItemCount(6);
     if (type === "all") return setFilteredType(cardInfo);
 
     const filteredData = cardInfo.filter(
@@ -60,7 +78,10 @@ export default function Team() {
 
     setFilteredType(filteredData);
   };
-
+  const handleViewMore = () => {
+    setVisibleItemCount((prevCount) => prevCount + 6);
+    // setDisplayedData(filteredType.slice(0, visibleItemCount + 5));
+  };
   return (
     <div className="">
       <NavBar />
@@ -137,7 +158,7 @@ export default function Team() {
           </button>
         </div>
         <CardContainer>
-          {filteredType.map((info) => (
+          {filteredType.slice(0, visibleItemCount).map((info) => (
             <Card
               key={info.id}
               image={info.image}
@@ -146,6 +167,27 @@ export default function Team() {
             />
           ))}
         </CardContainer>
+        {visibleItemCount < filteredType.length && (
+          <div className="w-fit m-auto" onClick={handleViewMore}>
+            <button
+              type="button"
+              className="bg-[#fe424c] px-4 py-2 rounded-md text-white hover:bg-opacity-70 "
+            >
+              View More
+            </button>
+          </div>
+        )}
+        {visibleItemCount >= filteredType.length && (
+          <div className="w-fit m-auto" onClick={handleViewMore}>
+            <button
+              type="button"
+              disabled
+              className="bg-[#fe424c] px-4 py-2 cursor-not-allowed rounded-md text-white bg-opacity-20 "
+            >
+              View More
+            </button>
+          </div>
+        )}
       </AlignContainer>
       <Footer />
     </div>
