900ba665303e7e2c84d04eb17de8c4212747bb1d
Mary Joe <maryjoekhoury@outlook.com>
2024-08-21 09:58:45 +0300

    added modal

--- components/FlipCard.js
+++ components/FlipCard.js
@@ -9,12 +9,12 @@ const FlipCard = (props) => {
       <Link href={props.link}>
         <div className="relative group">
           <img src={props.image} />
-          <h1 className="text-white absolute m-auto left-0 right-0  top-[42%]  md:text-2xl text-lg  text-center ">
+          <h1 className="text-white absolute m-auto left-0 right-0  top-0 bottom-0 w-fit h-fit  md:text-2xl text-lg  text-center ">
             {props.title}
           </h1>
 
           <div className="text-white absolute m-auto  right-4 bottom-0 text-lg  text-center ">
-            <h1 className="relative after:absolute after:h-[2px] after:w-0 after:-bottom-0 after:right-0 after:left-0 after:m-auto after:bg-[#fe424c] after:rounded-md group-hover:after:w-full translate-x-1 duration-150 hover:text-white  after:duration-150">
+            <h1 className="relative after:absolute after:h-[2px] after:w-0 after:-bottom-0 after:right-0 mb-2 after:left-0 after:m-auto  after:bg-[#fe424c] after:rounded-md group-hover:after:w-full translate-x-1 duration-150 hover:text-white  after:duration-150">
               See More
             </h1>
           </div>
--- pages/work.js
+++ pages/work.js
@@ -1,18 +1,41 @@
+import { useState } from "react";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import AlignContainer from "@/components/AlignContainer";
 import axios from "@/utils/axios";
+import Modal from "@/components/Modal";
 
 export default function Work({ metas, links }) {
+  const [showModal, setShowModal] = useState(false);
   return (
     <>
       <NavBar image={metas.data.seo_image} />
-      <AlignContainer>
-        <h1 className="text-white md:text-5xl text-3xl text-center my-28">
-          COMING SOON...
-        </h1>
-      </AlignContainer>
 
+      {/* <h1 className="text-white md:text-5xl text-3xl text-center my-28">
+          COMING SOON...
+        </h1> */}
+      <div className="relative">
+        <img src="/images/cam.webp" className="opacity-80 w-full h-full" />
+        <button
+          type="button"
+          onClick={() => setShowModal(true)}
+          className="text-white bg-[#fe424c]  hover:underline absolute md:px-8 px-4 text-lg py-2 rounded-lg left-0 right-0 m-auto w-fit top-0 bottom-0 h-fit"
+        >
+          Click
+        </button>
+        {showModal && <Modal onClose={() => setShowModal(false)} />}
+      </div>
+
+      {/* <AlignContainer>
+        <div className="bg-white">
+          <iframe
+            src="https://www.youtube.com/embed/c8C04rOeFvI&list"
+            frameborder="0"
+            allowfullscreen
+            loading="lazy"
+          ></iframe>
+        </div>
+      </AlignContainer> */}
       <Footer
         linkedin={links.data.social_media.en.Linkedin}
         instagram={links.data.social_media.en.Instagram}
